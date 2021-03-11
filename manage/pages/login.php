70a07ed30216978f24e6a80f8538ddfc353cea0d
shlomohass <shlomohassid@gmail.com>
2021-03-11 23:10:23 +0200

    now composer manages required platform admin panel libs

--- manage/pages/login.php
+++ manage/pages/login.php
@@ -1,3 +1,87 @@
 <?php 
 
 echo "admin login please";
+
+
+
+/*
+    "repositories": [
+        {
+            "type": "package",
+            "package": {
+                "name": "jquery/jquery",
+                "version": "1.2.3",
+                "dist": {
+                    "url": "http://code.jquery.com/jquery-3.5.1.min.js",
+                    "type": "file"
+                }
+            }
+        }
+    ]
+    */
+    
+?>
+<html data-docby="BSIK Platfom">
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="">
+        <meta name="author" content="SIKTEC">
+        <meta name="description" content="PHP made simple the correct way to build fast and powerful PHP web apps">
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+        <meta name="csrf-token" content="b342665c672cafd5586755b2fa34fbf476453e21cb5a19838b27de56858bfb7d">
+        <meta name="revisit-after" content="7 days">
+        <title>SIK Framework - Main page</title>
+        <link rel="icon" href="">
+        <!-- START : Head includes -->
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic" />
+        <link rel="stylesheet" href="/bsik/lib/import/bootstrap.5.0.0/css/bootstrap.min.css">
+        <script type="text/javascript" src="/bsik/lib/import/jquery.3.5.1/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="/bsik/lib/import/bootstrap.5.0.0/js/bootstrap.min.js"></script>
+
+        <!-- END : Head includes -->
+    </head>
+    <body style="">        
+        <!-- END : body includes -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-2"></div>
+                <div class="col-lg-6 col-md-8 login-box">
+                    <div class="col-lg-12 login-key">
+                        <i class="fa fa-key" aria-hidden="true"></i>
+                    </div>
+                    <div class="col-lg-12 login-title">
+                        ADMIN PANEL
+                    </div>
+
+                    <div class="col-lg-12 login-form">
+                        <div class="col-lg-12 login-form">
+                            <form>
+                                <div class="form-group">
+                                    <label class="form-control-label">USERNAME</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-control-label">PASSWORD</label>
+                                    <input type="password" class="form-control" i>
+                                </div>
+
+                                <div class="col-lg-12 loginbttm">
+                                    <div class="col-lg-6 login-btm login-text">
+                                        <!-- Error Message -->
+                                    </div>
+                                    <div class="col-lg-6 login-btm login-button">
+                                        <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-2"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
