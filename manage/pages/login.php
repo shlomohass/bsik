ade0414741d66de276fdbaa62fa724cac880e6a6
shlomohass <shlomohassid@gmail.com>
2021-03-12 01:34:27 +0200

    Created login form + page + removed manage fonts

--- manage/pages/login.php
+++ manage/pages/login.php
@@ -4,12 +4,20 @@ if (!isset($APage)) {
     $APage = new APage();
 } 
 
+$APage->include("head", "css", "path", ["name" => "lib/required/font-awesome/css/all.min.css"]);
 $APage->include("head", "css", "path", ["name" => "lib/required/bootstrap/css/bootstrap.css"]);
 $APage->include("head", "css", "path", ["name" => "lib/css/global.css"]);
 $APage->include("head", "css", "path", ["name" => "lib/css/login.css"]);
 $APage->include("head", "js",  "path", ["name" => "lib/required/jquery/jquery.min.js"]);
 
-/******************************  Guard  *****************************/
+
+/******************************  Basic login Form values  *****************************/
+$APage->store("form-title", "ADMIN PANEL LOGIN");
+$APage->store("plat-info", "BSik by SIKTEC - Version: ".APP_VERSION);
+$APage->store("form-user-label", "USERNAME");
+$APage->store("form-user-pass", "PASSWORD");
+$APage->store("form-btn-login", "LOGIN");
+
 
 ?>
 <html data-docby="BSIK Platfom">
@@ -35,37 +43,48 @@ $APage->include("head", "js",  "path", ["name" => "lib/required/jquery/jquery.mi
             <div class="row">
                 <div class="col-lg-3 col-md-2"></div>
                 <div class="col-lg-6 col-md-8 login-box">
-                    <div class="col-lg-12 login-key">
-                        <i class="fa fa-key" aria-hidden="true"></i>
+                    <div class="col-lg-12 plat-logo">
+                        <img src="lib/img/logo.svg" />
                     </div>
                     <div class="col-lg-12 login-title">
-                        ADMIN PANEL
+                        <?php print $APage->get("form-title"); ?>
                     </div>
 
                     <div class="col-lg-12 login-form">
                         <div class="col-lg-12 login-form">
-                            <form>
+                            <form method="post">
+                                <input type="hidden" name="csrftoken" value="<?php print $APage->token["csrf"]; ?>" />
                                 <div class="form-group">
-                                    <label class="form-control-label">USERNAME</label>
-                                    <input type="text" class="form-control">
+                                    <label class="form-control-label">
+                                        <i class='fas fa-user'></i>&nbsp;&nbsp;
+                                        <?php print $APage->get("form-user-label"); ?>
+                                    </label>
+                                    <input name="username" type="text" class="form-control" />
                                 </div>
                                 <div class="form-group">
-                                    <label class="form-control-label">PASSWORD</label>
-                                    <input type="password" class="form-control" i>
+                                    <label class="form-control-label">
+                                        <i class='fas fa-key'></i>&nbsp;&nbsp;
+                                        <?php print $APage->get("form-user-pass"); ?>
+                                    </label>
+                                    <input name="password" type="password" class="form-control" />
                                 </div>
-
-                                <div class="col-lg-12 loginbttm">
-                                    <div class="col-lg-6 login-btm login-text">
+                                <div class="col-lg-12">
+                                    <div class="col-12 login-text">
                                         <!-- Error Message -->
                                     </div>
-                                    <div class="col-lg-6 login-btm login-button">
-                                        <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                                    <div class="col-12 text-center mb-4">
+                                        <button type="submit" class="btn btn-outline-primary">
+                                            <?php print $APage->get("form-btn-login"); ?>
+                                        </button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-2"></div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+                <div class="col-12 plat-info">
+                    <?php print $APage->get("plat-info"); ?>
                 </div>
             </div>
         </div>
